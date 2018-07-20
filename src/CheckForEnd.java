c6022ecc50cabafec6a82c5a4927314bbaa3e4d1
Dan <da_nil@ukr.net>
2018-07-20 10:38:35 +0300

    ////

--- src/CheckForEnd.java
+++ src/CheckForEnd.java
@@ -60,4 +60,58 @@ public class CheckForEnd {
 
         assertTrue(vc.winner);
     }
+
+    @Test
+    void testCheckState5(){
+        vc.badModels.clear();
+        vc.eatModels.clear();
+        vc.c1 = new Circle1(1,1,48);
+        vc.eatModels.add(new EatModel(272,22,5,5));
+        vc.eatModels.add(new EatModel(372,22,5,5));
+        vc.eatModels.add(new EatModel(372,122,5,5));
+
+        vc.c1.speedX(250);
+        vc.checkForEnd();
+        assertFalse(vc.winner);
+
+        vc.c1.speedX(100);
+        vc.checkForEnd();
+        assertFalse(vc.winner);
+
+        vc.c1.speedX(-50);
+        vc.c1.speedY(100);
+        vc.checkForEnd();
+        assertFalse(vc.winner);
+
+        vc.c1.speedX(50);
+        vc.checkForEnd();
+        assertTrue(vc.winner);
+    }
+
+    @Test
+    void testCheckState6(){
+        vc.badModels.clear();
+        vc.eatModels.clear();
+        vc.c1 = new Circle1(1,1,48);
+        vc.eatModels.add(new EatModel(272,22,5,5));
+        vc.eatModels.add(new EatModel(372,22,5,5));
+        vc.eatModels.add(new EatModel(272,122,5,5));
+
+        vc.c1.speedX(250);
+        vc.checkForEnd();
+        assertFalse(vc.winner);
+
+        vc.c1.speedX(100);
+        vc.checkForEnd();
+        assertFalse(vc.winner);
+
+        vc.c1.speedX(-50);
+        vc.c1.speedY(100);
+        vc.checkForEnd();
+        assertFalse(vc.winner);
+
+        vc.c1.speedX(-50);
+        vc.checkForEnd();
+        assertTrue(vc.winner);
+    }
 }
